24f34c33399c823d64aead92f899d853147a2176
Juan <Juan@DESKTOP-HAQV61S>
2017-12-02 18:57:49 -0500

    v2

---
+++ src/main/java/ec/ups/edu/appdis/marketbit1/modelo/categoria.java
@@ -0,0 +1,30 @@
+package ec.ups.edu.appdis.marketbit1.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class categoria {
+	
+	@Id
+	private int codigo;
+	@NotNull
+	@Column(name="cat_nom",length=30)
+	private String nombre;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
