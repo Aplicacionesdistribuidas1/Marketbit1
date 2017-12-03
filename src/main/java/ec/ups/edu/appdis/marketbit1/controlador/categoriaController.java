1b024da5d3b5e32f56b219d693ca2ac48279113c
Juan <Juan@DESKTOP-HAQV61S>
2017-12-02 22:36:51 -0500

    v11

--- src/main/java/ec/ups/edu/appdis/marketbit1/controlador/categoriaController.java
+++ src/main/java/ec/ups/edu/appdis/marketbit1/controlador/categoriaController.java
@@ -1,13 +1,78 @@
 package ec.ups.edu.appdis.marketbit1.controlador;
+import ec.ups.edu.appdis.marketbit1.modelo.categoria;
+import ec.ups.edu.appdis.marketbit1.datos.categoriaDAO;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
-import ec.ups.edu.appdis.marketbit1.datos.categoriaDAO;
+
 
 @ManagedBean
 public class categoriaController {
 	@Inject
 	private categoriaDAO cadao;
+	private categoria categoria= null;
+	private List<categoria> categorias;
+	
+	private int id;
+	
+	@PostConstruct
+	public void init() {
+		categoria = new categoria();
+		loadCategoria();
+	}
 
+	public categoriaDAO getCadao() {
+		return cadao;
+	}
+
+	public void setCadao(categoriaDAO cadao) {
+		this.cadao = cadao;
+	}
+
+	public categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+		loadDatosEditar(id);
+	}
+	public void loadCategoria() {
+		categorias = cadao.listadoCategoria();
+	}
+	
+	public String guardar() {
+		cadao.guardar(categoria);
+		loadCategoria();
+		return "listadoCategorias";
+	}
+	public String loadDatosEditar(int id) {
+		System.out.println("cargando datos de categoria a editar"+ id);
+		categoria= cadao.leer(id);
+		return "FicheroCategoria";
+	}
+	public String eliminarDatos(int id) {
+		cadao.borrar(id);
+		return "listadoCategorias";
+	}
 }
