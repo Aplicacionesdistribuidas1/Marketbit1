ac78233f51448b431fd1b4846b782a4539e79fc7
Francisco Cherrez <Francisco Cherrez@DESKTOP-T4314D2>
2017-12-02 22:40:27 -0500

    c3

---
+++ src/main/java/ec/ups/edu/appdis/marketbit1/controlador/fac_cabeceraController.java
@@ -0,0 +1,48 @@
+package ec.ups.edu.appdis.marketbit1.controlador;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import ec.ups.edu.appdis.marketbit1.datos.fac_cabeceraDAO;
+
+@ManagedBean
+public class fac_cabeceraController {
+	
+	@Inject
+	private fac_cabeceraDAO fcdao;
+	
+
+	private fac_cabecera fac_cabecera = null;
+	private List<fac_cabecera> fac_cabecera;
+	
+	private int id;
+	
+	@PostConstruct
+	public void init() {
+		fac_cabecera = new fac_cabecera();
+		loadfac_cabecera();
+	}
+	
+	public fac_cabecera getfac_cabecera() {
+		return fac_cabecera;
+	}
+	
+	public void setfac_cabecera(fac_cabecera fac_cabecera) {
+		
+		this.fac_cabecera = fac_cabecera;
+		
+	} 
+	
+	public void loadfac_cabecera() {
+		fac_cabecera = fcdao.listadofacCabecera();
+	}
+	
+	public String guardar() {
+		fcdao.guardar(fac_cabecera);
+		loadfac_cabecera();
+		return "listadofacCabecera";
+	}
+	
+	
+}
--- src/main/java/ec/ups/edu/appdis/marketbit1/modelo/fac_cabecera.java
+++ src/main/java/ec/ups/edu/appdis/marketbit1/modelo/fac_cabecera.java
@@ -1,6 +1,63 @@
 package ec.ups.edu.appdis.marketbit1.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
 public class fac_cabecera {
+	
+	@Id
+	@Column(name="facC_codigo")
+	private int codigo;
+	
+	@Column(name="facC_numero_factura")
+	@Size(min=4,max=20)
+	private int numero_factura;
+	
+	@Column(name="facC_total")
+	@Size(min=2,max=20)
+	private double total;
+	
+	@Column(name="facC_iva")
+	@Size(min=2,max=10)
+	private double iva;
+	
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getNumero_factura() {
+		return numero_factura;
+	}
+
+	public void setNumero_factura(int numero_factura) {
+		this.numero_factura = numero_factura;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	
+	
 	
 	
 
