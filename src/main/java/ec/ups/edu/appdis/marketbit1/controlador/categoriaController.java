f61edeb0547c2774ff466132f47a2385715e8cd4
Juan <Juan@DESKTOP-HAQV61S>
2017-12-02 21:07:04 -0500

    v6

--- src/main/java/ec/ups/edu/appdis/marketbit1/controlador/categoriaController.java
+++ src/main/java/ec/ups/edu/appdis/marketbit1/controlador/categoriaController.java
@@ -1,5 +1,11 @@
 package ec.ups.edu.appdis.marketbit1.controlador;
 
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+@ManagedBean
 public class categoriaController {
+	@Inject
+	private categoriaDAO cadao;
 
 }
--- src/main/java/ec/ups/edu/appdis/marketbit1/datos/categoriaDAO.java
+++ src/main/java/ec/ups/edu/appdis/marketbit1/datos/categoriaDAO.java
@@ -1,5 +1,8 @@
 package ec.ups.edu.appdis.marketbit1.datos;
 
+import javax.ejb.Stateless;
+
+@Stateless
 public class categoriaDAO {
 
 }
