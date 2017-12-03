18974ff14ab623d906736c2db8beb0395f732206
Juan <Juan@DESKTOP-HAQV61S>
2017-12-02 19:23:21 -0500

    v4

--- src/main/java/ec/ups/edu/appdis/marketbit1/modelo/producto.java
+++ src/main/java/ec/ups/edu/appdis/marketbit1/modelo/producto.java
@@ -1,5 +1,27 @@
 package ec.ups.edu.appdis.marketbit1.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class producto {
+	@Id
+	private int codigo;
+	@NotNull
+	@Column(name="pro_nombre", length=50)
+	private String nombre;
+	@NotNull
+	@Column(name="pro_precio", length=10)
+	private double precio;
+	@NotNull
+	@Column(name="pro_descripcion", length=10)
+	private String descripcion;
+	@NotNull
+	@Column(name="pro_stock", length=10)
+	private int stock;
+	
+	
 
 }
