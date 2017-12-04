7f32a212a2583e89f601e999a52ede95fe904229
Daniel Pelaez <Daniel Pelaez@DESKTOP-5ON8VO2>
2017-12-04 03:05:03 -0500

    V43

---
+++ src/main/java/ec/ups/edu/appdis/marketbit1/modelo/Empleado.java
@@ -0,0 +1,100 @@
+package ec.ups.edu.appdis.marketbit1.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Empleado {
+	
+	@Id
+	@Column(name = "emp_codigo")
+	private int codigo;
+	
+	@NotNull
+	@Column(name = "emp_cedula", length = 10)
+	@Size(max = 10)
+	private String cedula;
+	
+	@NotNull
+	@Column(name = "emp_nombre")
+	@Size(min = 4, max = 20)
+	private String nombre;
+	
+	@NotNull
+	@Column(name = "emp_usuario")
+	@Size(min = 4, max = 20)
+	private String usuario;
+	
+	@NotNull
+	@Column(name = "emp_contrasena")
+	@Size(min = 4, max = 20)
+	private String contrasena;
+	
+	@NotNull
+	@Column(name = "emp_cargo")
+	@Size(min = 4, max = 20)
+	private String cargo;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [codigo=" + codigo + ", cedula=" + cedula + ", nombre=" + nombre + ", usuario=" + usuario
+				+ ", contrasena=" + contrasena + ", cargo=" + cargo + "]";
+	}
+
+	
+	
+	
+	
+
+}
